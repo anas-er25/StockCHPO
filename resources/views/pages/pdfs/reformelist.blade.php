5c290eaa02e60a05fa7df09a8c30e0cb39b77082
Anas ER-RAKIBI <errakibianasdev@gmail.com>
2025-01-29 23:32:08 +0100

    add header and footer to pdfs

--- resources/views/pages/pdfs/reformelist.blade.php
+++ resources/views/pages/pdfs/reformelist.blade.php
@@ -8,11 +8,46 @@
     <style>
         body {
             font-family: Arial, sans-serif;
+            font-size: 12px;
+            margin: 20px;
         }
 
-        header {
+        .header {
             text-align: center;
-            margin-bottom: 20px;
+            font-size: 18px;
+            font-weight: bold;
+            margin-bottom: 30px;
+        }
+
+        header h2,
+        header h3 {
+            text-align: center;
+            margin: 5px 0;
+            /* Espacement entre les éléments */
+        }
+
+        .header img {
+            width: 100%;
+            /* Ajustez la largeur de l'image de l'en-tête */
+            max-width: 800px;
+            /* Limitez la largeur maximale */
+            height: 100px;
+            /* Gardez le ratio de l'image */
+        }
+
+        .footer img {
+            width: 100%;
+            /* Ajustez la largeur de l'image du pied de page */
+            max-width: 800px;
+            /* Limitez la largeur maximale */
+            height: 80px;
+            /* Gardez le ratio de l'image */
+            margin-top: 20px;
+            /* Espacement au-dessus du pied de page */
+        }
+
+        .right {
+            text-align: right;
         }
 
         table {
@@ -39,13 +74,63 @@
         .designation {
             width: 40%;
         }
+
+        .signature {
+            display: flex;
+            justify-content: space-between;
+            margin-top: 30px;
+            width: 100%;
+        }
+
+        .signature div {
+            text-align: center;
+            margin-top: 10px;
+        }
+
+        .signature div:nth-child(1) {
+            text-align: left;
+            margin-top: 20px;
+        }
+
+        .signature div:nth-child(2) {
+            text-align: right;
+            margin-top: 20px;
+        }
+
+        .signature div:nth-child(3),
+        .signature div:nth-child(4) {
+            text-align: center;
+            margin-top: 100px;
+        }
+
+        .subsignature {
+            margin-top: 50px;
+        }
+
+        .subsignature h4 {
+            margin-bottom: 10px;
+        }
+
+        .subsignature ul {
+            padding: 5px;
+        }
+
+        .footer {
+            text-align: center;
+            margin-top: 100px;
+            font-size: 10px;
+        }
     </style>
 </head>
 
 <body>
-    <header>
-        <h1>Liste du matériel proposé à la réforme du service: ............... année :
-            {{ date('Y') }}/{{ date('Y') + 1 }}</h1>
+    <!-- En-tête avec l'image header.png -->
+    <div class="header">
+        <img src="{{ public_path('assets/images/pdf/header.png') }}" alt="En-tête">
+    </div>
+    <header >
+        <h2>Liste du matériel proposé à la réforme</h2>
+        <h3>Service/Unité : ................ Année : {{ date('Y') }}/{{ date('Y') + 1 }}</h3>
     </header>
     <table>
         <thead>
@@ -123,56 +208,17 @@
                 <td></td>
                 <td></td>
             </tr>
-            <tr>
-                <td class="designation"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td class="designation"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td class="designation"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td class="designation"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td class="designation"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td class="designation"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td class="designation"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td class="designation"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+
         </tbody>
     </table>
+    <div class="signature">
+        <div>Signature </div>
+        <div>le : ................</div>
+    </div>
+    <!-- Pied de page avec l'image footer.png -->
+    <div class="footer">
+        <img src="{{ public_path('assets/images/pdf/footer.png') }}" alt="Pied de page">
+    </div>
 </body>
 
 </html>
